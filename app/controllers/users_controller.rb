0460947e20d5169e456cdc7fbd90de2ecf7e174a
Aritro Roy <arrxy@Aritros-MacBook-Pro.local>
2022-07-06 14:43:16 +0530

    User log-in and restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @articles = Article.all
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = 'Welcome'
       redirect_to articles_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
-  resources :articles
-  get 'signup' => 'users#new'
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get        'signup' =>  'users#new'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get        'login'  =>  'sessions#new'
+  post       'login'  =>  'sessions#create'
+  get        'logout' =>  'sessions#destroy'
+  #delete
+
+  resources :articles
+  resources :users
+
 end
